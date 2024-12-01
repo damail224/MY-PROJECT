146245b474599c6ebcc98e5d6dd23adc1c021e84
damail224 <mirriamkawira728@gmail.com>
2024-12-01 16:59:39 +0300

    ui changes

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ def home():
 
 @app.route('/menu')   
 def menu():
-    return render_template("menu.html") 
+    return render_template("menu.html")
 
-@app.route("/upload" , methods=["POST","GET"])
-def upload():
+@app.route('/place_order')   
+def place_order():
+    return render_template("place_order.html") 
+
+@app.route('/cart')   
+def cart():
+    return render_template("cart.html")
+
+@app.route("/uploadnew" , methods=["POST","GET"])
+def uploadnew():
     if request.method == "GET":
-     return render_template("upload.html")
+     return render_template("uploadnew.html")
     else:
         foodname = request.form ["foodname"]
         description = request.form ["description"]
@@ -24,13 +32,13 @@ def upload():
 
         connection = pymysql.connect(host="localhost" , user="root" , password="" ,database="keroma")
 
-        sql = "INSERT INTO menu-items ( 'foodname', 'description', 'price', 'image_name') VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO `menu_items`( `foodname`, `description`, `price`, `image_name`) VALUES (%s, %s, %s, %s)"
 
         cursor = connection.cursor()
         cursor.execute(sql, (foodname, description, price, image_name.filename))
         
         connection.commit()
-        return render_template("upload.html", message =" Uploaded Succesfully")
+        return render_template("uploadnew.html", message =" Uploaded Succesfully")
     
 
 app.run(debug=True)
